d7073544e815f3eb2adc3fa56c7b2d27b1771fd1
Harsh Kumar Raghav <72187191+HarshKumarraghav@users.noreply.github.com>
2022-01-25 03:56:38 +0530

    commit

---
+++ precache-manifest.dcb5f9b931ca0fb124c3fb27cc509d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac12d32855027d25bbdccd679234398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ddfc92fe1066e38188a",
+    "url": "/static/css/2.e3d3c5aa.chunk.css"
+  },
+  {
+    "revision": "20e2f2573f4927e69efc",
+    "url": "/static/css/main.79ac1011.chunk.css"
+  },
+  {
+    "revision": "9ddfc92fe1066e38188a",
+    "url": "/static/js/2.7797597b.chunk.js"
+  },
+  {
+    "revision": "20e2f2573f4927e69efc",
+    "url": "/static/js/main.daea588f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/static/media/cryptocurrency.88532732.png"
+  }
+]);
